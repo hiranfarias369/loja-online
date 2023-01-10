c4c1a4b748d60b79fbc57a0e2facaba0569f4f81
hiranfarias369 <hiran.farias.lima@gmail.com>
2023-01-09 22:27:51 -0300

    first commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 //Create Product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -14,7 +15,11 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get All Products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-	const products = await Product.find();
+	const apiFeature = new ApiFeatures(Product.find(), req.query)
+		.search()
+		.filter();
+
+	const products = await apiFeature.query;
 
 	res.status(200).json({ sucess: true, products });
 });
